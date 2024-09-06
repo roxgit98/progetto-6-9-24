7302d6b3b42df4ad1765ed580f915e781f2f5cdf
Davide Giliberti <gilo.davide@hotmail.it>
2024-09-06 11:42:59 +0200

    initial commit

---
+++ src/main/java/davidegiliberti/progetto_6_9_24/Progetto6924Application.java
@@ -0,0 +1,13 @@
+package davidegiliberti.progetto_6_9_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto6924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progetto6924Application.class, args);
+	}
+
+}
---
+++ src/test/java/davidegiliberti/progetto_6_9_24/Progetto6924ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidegiliberti.progetto_6_9_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progetto6924ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
